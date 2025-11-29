6c65e2c448dd7a489e28bfc23d7c0212c88f59c9
lqw2g-commit <jxckjpwc2g@privaterelay.appleid.com>
2025-11-29 09:07:10 +0800

    Update Main.cpp

--- WorldSvr/Main.cpp
+++ WorldSvr/Main.cpp
@@ -1,22 +1,75 @@
-﻿#include <iostream>
-#include <Core.h>
-#include <Proc/Proc.h>
-#include "Memory/Memory.h"
-#include "Game/MacroBM3/MacroBM3.h"
-#include "Game/EventAlert/EventAlert.h"
-#include "Game/Warp/Warp.h"
-#include "Game/WindowList/WindowList.h"
+#include "Base.h"
+#include "Game/Game.h"
+#include "PacketManager/PacketManager.h"
+#include "GameVersion/GameVersion.h"
+#include "Basic/Basic.h"
+#include "Discord/Discord.h"
+#include "DLLSec/DLLSec.h"
+#include "Screen/Screen.h"
+#include "WndProc/WndProc.h"
+#include "Proc/Proc.h"
+#include "GameCustom/MacroBM3/BM3Macro.h"
+#include "GameCustom/BattleMode/Battlemode.h"
+#include <iostream>
+#include <windows.h>
 
-void LoadConfigs()
+// Example exported function
+extern "C" __declspec(dllexport) void Episode33()
 {
-    INIT(Proc);
-    INIT(Warp);
-    EventAlert::Initialize();
-    MacroBM3::Initialize();
-    WindowList::Initialize();
+    std::cout << "Exported function called!" << std::endl;
 }
 
-extern "C" void Init()
+void InitAll()
 {
-    LoadConfigs();
+    // Existing initialization logic
+    VM_STARTU(__FUNCTION__);
+    g_Procedures->SetHooks();
+    g_pBM3Macro->Init();
+
+    WindowProc::InitHooks();
+    //Game::MemoryChecks::InitMemoryChanges();
+    DirectX::InitHooks();
+    GameVersion::SetHooks();
+    PacketManager::SetHooks();
+    Basic::SetHooks();
+    Discord::SetHooks();
+    DLLSec::SetHooks();
+    Screen::InitHooks();
+    BattleMode::SetHooks();
+
+
+
+    VM_END;
+}
+
+#ifdef DEV_MODE
+void allocConsole()
+{
+    AllocConsole();
+
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+
+    std::wcout.clear();
+    std::cout.clear();
+    std::wcerr.clear();
+    std::cerr.clear();
+    std::wcin.clear();
+    std::cin.clear();
+}
+#endif
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+#ifdef DEV_MODE
+        allocConsole();
+#endif
+        InitAll();
+        break;
+    }
+    return TRUE;
 }
